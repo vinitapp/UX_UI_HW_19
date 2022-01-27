088f75beaa9b90d02d72e1ffe2b268fd339fcba0
Vinita <vinitaprabhu10@gmail.com>
2022-01-27 18:06:32 +1100

    first commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  $("button").hover(function () {
+    $(this).css("background-color", "#04AA6D");
+  }, function () {
+    $(this).css("background-color", "black");
+  });
+});
+
+$(document).ready(function () {
+  $(".button2").hover(function () {
+    $(this).css("background-color", "#F2CF59");
+  }, function () {
+    $(this).css("background-color", "black");
+  });
+});
+
+// $('#animate-demo1').on('click', function () {
+
+//   if (!$(this).is(':animated')) {
+//     $(".complete").remove();
+//     $(this).after("<div class='ani-status'>The image is animating</div>");
+//   }
+
+//   $(this).rotate(-1500, {
+//     duration: 1500,
+//     easing: 'easeOutExpo',
+//     complete: function () {
+//       $(".ani-status").remove();
+//       $(this).after("<div class='complete'>Animation is done!</div>");
+//     }
+//   });
+
+// });
+
+// var tmpAnimation = 0;
+// $('#animate-demo1').on('click', function (){
+//   var element = $("img");
+//   tmpAnimation = tmpAnimation + 45;
+  
+//   $({degrees: tmpAnimation - 45}).animate({degrees: tmpAnimation}, {
+//       duration: 3000,
+//       step: function(now) {
+//           element.css({
+//               transform: 'rotate(' + now + 'deg)'
+//           });
+//       }
+//   });
+// });
+
+function RotateImageMethod(degree) {
+	$('#imageSec').animate({  transform: degree }, 
+	{
+    step: function(now,fx) 
+    {
+        $(this).css({
+            '-webkit-transform':'rotate('+now+'deg)',
+            '-moz-transform':'rotate('+now+'deg)',
+            'transform':'rotate('+now+'deg)'
+        });
+    }
+    });
+}
